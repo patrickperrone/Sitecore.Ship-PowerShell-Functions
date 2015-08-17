cb6566803a01aff51d46ced30c430573c03b03f9
Patrick Perrone <patrickperrone@users.noreply.github.com>
2015-08-17 11:24:33 -0400

    Update chocolateyInstall.ps1

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $name = "SitecoreShipFunctions"
-$url = "https://github.com/patrickperrone/Sitecore.Ship-PowerShell-Functions/archive/v1.0.1.zip"
+$url = "https://github.com/patrickperrone/Sitecore.Ship-PowerShell-Functions/archive/v1.1.0.zip"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage $name $url $unzipLocation
 
-$installer = Join-Path $unziplocation 'Sitecore.Ship-PowerShell-Functions-1.0.1\Install.ps1'
+$installer = Join-Path $unziplocation 'Sitecore.Ship-PowerShell-Functions-1.1.0\Install.ps1'
 & $installer
